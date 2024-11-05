43a0c334ab3110fc13d4149c125a11005adb8b05
archita-b <archita.bhattacharyya91@gmail.com>
2024-11-05 21:53:16 +0530

    implement messages scroll to last message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import useGetMessages from "../../hooks/useGetMessages";
 import useListenMessages from "../../hooks/useListenMessages";
 import Message from "./Message";
@@ -5,6 +7,14 @@ import Message from "./Message";
 const Messages = () => {
   const { loading, messages } = useGetMessages();
   useListenMessages();
+
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
 
   return (
     <div className="px-4 flex-1 overflow-auto">
